38c5706926af3fd067a48e98af996dd07f9f34b1
Bryn Heath <brynheath1@gmail.com>
2020-01-19 17:33:06 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
-
+################################################################################
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -11,7 +11,7 @@ def directors_totals(source)
   end
   result
 end
-
+################################################################################
 def gross_for_director(d)
   total = 0
   index = 0
@@ -23,21 +23,41 @@ def gross_for_director(d)
 
   total
 end
-
+################################################################################
 def list_of_directors(source)
-  # Write this implementation
+arr = []
+i=0
+  while i < source.size do
+  arr << source[i][:name]
+  i += 1
+  end
+  #puts arr
+  #pp arr
+  return arr
 end
-
+################################################################################
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+names = list_of_directors(source)
+part1 = directors_totals(source)
+i=0
+total = 0
+  while i < names.size do
+  total += part1[names[i]]
+  puts total
+  i +=1
+  end
+  puts total
+return total
 end
-
-
+################################################################################
+# Write this implementation
+#
+# Should use methods:
+# 1. directors_totals: returns a Hash of { dir_name => gross }
+# 2. list_of_directors: names provides an Array of directors names (use
+#
+# Visit each key (i.e. director name), look up the value in the hash
+# returned by directors_totals, and add it to a running total. When done,
+# return the total
+#
+#
